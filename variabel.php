509d874a673a059d0df782703d19a5ff96571588
Feb11f <yayanfebri43@gmail.com>
2022-03-12 22:58:34 +0700

    menambahkan variabel.php

---
+++ variabel.php
@@ -0,0 +1,17 @@
+<?php
+$a = 5;
+$b = 5;
+$c = 5;
+
+//penjumlahan
+$hasilPenjumlahan = $a + $b;
+echo "hasil penjumlahan : $hasilPenjumlahan </br>"; //hasil penjulahan
+
+//pengurangan
+$hasilPengurangan = $c - $a;
+echo "hasil pengurangan : $hasilPengurangan </br>"; //hasil pengurangan
+
+//perkalian
+$hasilPerkalian = $b * $a;
+echo "hasil perkalian : $hasilPerkalian </br>"; //hasil perkalian
+?>
