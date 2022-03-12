f573763371ac280022affb5dd28c54e65dc08071
Feb11f <yayanfebri43@gmail.com>
2022-03-12 22:59:58 +0700

    menambahkan fungsi.php

---
+++ fungsi.php
@@ -0,0 +1,13 @@
+<?php
+//pembuatan fungsi
+function persegi($sisi)
+{
+   return $sisi*$sisi;
+}
+//pemanggilan fungsi
+echo "Luas persegi dengan sisi 7cm = ".persegi(7)."cm";
+echo "<br />";
+echo "Luas persegi dengan sisi 8cm = ".persegi(8)."cm";
+echo "<br />";
+echo "Luas persegi dengan sisi 9cm = ".persegi(9)."cm";
+?>
